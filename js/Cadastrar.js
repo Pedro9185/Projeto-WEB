2085ed54a1ce851e76ab51d699dcba172ad99e9a
Pedro9185 <pedrocatarina.ip@gmail.com>
2025-03-06 22:26:03 -0300

    feat: adicionado lógica de login e de cadastro

--- js/Cadastrar.js
+++ js/Cadastrar.js
@@ -1,3 +1,57 @@
+let usuarios = buscarDados("usuarios");
+
+const formulario = document.getElementById("formulario");
+const nome = document.getElementById("nome");
+const email = document.getElementById("email");
+const senha = document.getElementById("password");
+
+formulario.addEventListener("submit", (ev) => {
+  ev.preventDefault();
+
+  let existe = usuarios.some((v) => v.email === email.value);
+
+  if (existe) {
+    alert(
+      "Esse email já está cadastrado, verifique seu email e tente novamente"
+    );
+
+    return;
+  }
+  const novoUsuario = {
+    nome: nome.value,
+    email: email.value,
+    senha: senha.value,
+  };
+
+  usuarios.push(novoUsuario);
+
+  armazenar_dados("usuarios", usuarios);
+
+  formulario.reset();
+  alert("Usuario cadastrado");
+});
+
+function armazenar_dados(chave, valor) {
+  const dados = JSON.stringify(valor);
+  localStorage.setItem(chave, dados);
+}
+
+function buscarDados(chave) {
+  const dadosJSON = localStorage.getItem(chave);
+  if (dadosJSON) {
+    try {
+      const dadosConvertidos = JSON.parse(dadosJSON);
+      // Garante que o valor retornado seja um array
+      return Array.isArray(dadosConvertidos) ? dadosConvertidos : [];
+    } catch (e) {
+      console.error("Erro ao converter dados do localStorage:", e);
+      return [];
+    }
+  } else {
+    return [];
+  }
+}
+
 function ir_para_login() {
   window.location = `./Login.html`;
 }
--- js/Login.js
+++ js/Login.js
@@ -1,3 +1,45 @@
+let usuarios = buscarDados("usuarios");
+
+const formulario = document.getElementById("formulario");
+
+formulario.addEventListener("submit", (ev) => {
+  ev.preventDefault();
+  const email = document.getElementById("email").value;
+  const senha = document.getElementById("password").value;
+
+  const encontrarUsuario = usuarios.find(
+    (v) => v.email === email && v.senha === senha
+  );
+
+  if (!encontrarUsuario) {
+    alert("Email ou Senha incorretos ou não existem");
+  } else {
+    armazenar_dados("usuarioLogado", encontrarUsuario);
+    window.location = `./Home.html`;
+  }
+});
+
 function ir_para_pag_cadastrar() {
   window.location = "./Cadastrar.html";
+}
+
+function armazenar_dados(chave, valor) {
+  const dados = JSON.stringify(valor);
+  localStorage.setItem(chave, dados);
+}
+
+function buscarDados(chave) {
+  const dadosJSON = localStorage.getItem(chave);
+  if (dadosJSON) {
+    try {
+      const dadosConvertidos = JSON.parse(dadosJSON);
+      // Garante que o valor retornado seja um array
+      return Array.isArray(dadosConvertidos) ? dadosConvertidos : [];
+    } catch (e) {
+      console.error("Erro ao converter dados do localStorage:", e);
+      return [];
+    }
+  } else {
+    return [];
+  }
 }
